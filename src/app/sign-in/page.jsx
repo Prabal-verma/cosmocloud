21e8a130ac1094e0d7eb8008434881a1bbb73672
Prabal Verma <prableverma@gmail.com>
2024-10-13 18:35:40 +0530

    signin page

--- src/app/sign-in/page.jsx
+++ src/app/sign-in/page.jsx
@@ -4,7 +4,6 @@ import AuthStatus from '@/components/authstatus';
 const AuthPage = () => {
   return (
     <div>
-      <h1>Firebase Google Authentication</h1>
       <AuthStatus/>
     </div>
   );
